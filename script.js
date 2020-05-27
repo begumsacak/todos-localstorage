085ac6f6665f9b6a5fdf63d8be78477cbef72abd
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-05-26 20:45:05 -0500

    fixed an error

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
-//whenever we render to-dos, we want to empty it and render the list
-function renderTodos() {
-    todoList.innerHTML= "";
-    todoCountSpan.textContent = todos.length;
-//inside the reader function is the for loop
-    for var i=0; i<todos.length; i++ {
-    //create an element called li
-       var li = document.createElement("li");
-       //setting it up to the current element we are on
-        li.innerText = "todos[i]"; 
-        todoList.appendChild(li);
-    }
-};
 
-renderTodos()
+renderTodos();
+
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+  }
+}
